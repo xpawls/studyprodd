944f0cf02d185ac2ad1e1f9357197a8c8e49b69a
xpawls <minidice0114@gmail.com>
2017-09-21 18:09:16 +0900

    921

---
+++ JAVA/Work/JavaApplication/src/main/java/java04/ex03_10_LowerUpper.java
@@ -0,0 +1,19 @@
+package java04;
+
+import java.util.Scanner;
+
+public class ex03_10_LowerUpper {
+    
+    public static void main(String[] args){
+        
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("문자를 하나 입력(ex..a) : ");
+        String s = keyboard.next();
+        
+        char c = s.charAt(0);
+        
+        
+        
+    }
+    
+}
