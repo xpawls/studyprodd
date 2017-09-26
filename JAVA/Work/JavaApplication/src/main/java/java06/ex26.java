3147e90a0273ce2dc9ad1e45ade864f789f6ae98
xpawls <minidice0114@gmail.com>
2017-09-26 18:02:33 +0900

    aa

--- JAVA/Work/JavaApplication/src/main/java/java06/ex26.java
+++ JAVA/Work/JavaApplication/src/main/java/java06/ex26.java
@@ -1,29 +1,11 @@
 package java06;
 
+import java.util.Arrays;
+
 public class ex26 {
     public static void main(String[] args){
         
-        int[] i = {3,4,5,3,2};
-        
-        String str = "won";
-        
-        String result = inputVal(ii[5], str);
-        
-        System.out.println(result);
-        
         
     }
-    public static String[] inputVal(int[] ii, String ss){
-        
-        String sum = null;
-        String is = null;
-        for(int i=0; i<ii.length; i++){
-            is = is + (char)ii[i];
-        }
-        
-        
-        
-        return sum = ss + is;
-
-    }
+    
 }
---
+++ JAVA/Work/JavaApplication/src/main/java/java07/ex07_02_inputVal.java
@@ -0,0 +1,22 @@
+package java07;
+
+public class ex07_02_inputVal {
+    public static void main(String[] args){
+        String str = "hello";
+        
+        String[] starr = new String[4];
+        
+        String[] arr = inputVal(str);
+
+        System.out.println(arr[0]);
+        
+    }
+    public static String[] inputVal(String args){
+        
+        String[] result =null;
+        
+        return result;
+        
+    }
+
+}
